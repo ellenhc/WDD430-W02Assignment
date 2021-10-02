747a4390cc9664f47ae2c42811ae533d4b4f4d65
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2021-10-02 10:13:19 -0400

    Can switch between views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedFeature: string = 'documents';
+
+  switchView(selectedFeature: string){
+    this.selectedFeature = selectedFeature;
+  }
 }
