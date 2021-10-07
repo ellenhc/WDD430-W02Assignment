5d757d2367a207b4f753aa40d50a277cc592ea9c
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2021-10-07 12:37:56 -0500

    Finished W04 instructions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DocumentItemComponent } from './documents/document-list/document-item/d
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
 import { MessageItemComponent } from './messages/message-list/message-item/message-item.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { MessageItemComponent } from './messages/message-list/message-item/messa
     DocumentItemComponent,
     MessageListComponent,
     MessageEditComponent,
-    MessageItemComponent
+    MessageItemComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document("1", "1st Document", "This is a description.", "https://www.google.com/", null),
+    new Document("2", "2nd Document", "This is a description.", "https://www.google.com/", null),
+    new Document("3", "3rd Document", "This is a description.", "https://www.google.com/", null),
+    new Document("4", "4th Document", "This is a description.", "https://www.google.com/", null)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document){
+    /*Emit the selectedDocumentEvent and pass it 
+    the document object passed into the method.*/
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
