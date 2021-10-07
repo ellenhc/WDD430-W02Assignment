0b17c3592e027584401ab22e37f1e3cc2e75712c
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2021-10-07 12:39:10 -0500

    Change document-item descriptions

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -10,10 +10,10 @@ export class DocumentListComponent implements OnInit {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
   documents: Document[] = [
-    new Document("1", "1st Document", "This is a description.", "https://www.google.com/", null),
-    new Document("2", "2nd Document", "This is a description.", "https://www.google.com/", null),
-    new Document("3", "3rd Document", "This is a description.", "https://www.google.com/", null),
-    new Document("4", "4th Document", "This is a description.", "https://www.google.com/", null)
+    new Document("1", "1st Document", "This is the first description.", "https://www.google.com/", null),
+    new Document("2", "2nd Document", "This is the second description.", "https://www.google.com/", null),
+    new Document("3", "3rd Document", "This is the third description.", "https://www.google.com/", null),
+    new Document("4", "4th Document", "This is the fourth description.", "https://www.google.com/", null)
   ];
 
   constructor() { }
