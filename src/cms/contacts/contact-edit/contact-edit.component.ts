0674086db138d290224c023257d9038705f9166d
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2021-11-06 11:27:44 -0500

    Implement Behavior of Contact Edit Form

--- src/cms/contacts/contact-edit/contact-edit.component.ts
+++ src/cms/contacts/contact-edit/contact-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Contact } from '../contact.model';
 import { ContactService } from '../contact.service';
 
@@ -20,13 +20,46 @@ export class ContactEditComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id = params['id'];
+        if (!params.id) {
+          this.editMode = false;
+          return;
+        } //endif
+        this.originalContact = this.contactService.getContact(id);
+
+        if (!this.originalContact) {
+          return;
+        } //endif
+        this.editMode = true;
+        this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+        //if the contact has a group (not null)
+        if (this.contact.group) {
+          this.groupContacts = this.contact.group;
+        } //endif
+      })
   }
 
-  onRemoveItem(i){}
+  onRemoveItem(i) { }
 
-  onCancel(){}
+  onCancel() {
+    this.router.navigateByUrl('/contacts');
+  }
 
-  onSubmit(form: NgForm){}
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newContact = new Contact(value.id, value.name, value.email, value.phone, value.imageUrl, this.groupContacts);
+    if (this.editMode) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    }
+    else {
+      this.contactService.addContact(newContact);
+    }
+    // route back to '/documents' URL
+    this.router.navigateByUrl('/contacts');
+  }
 
 }
