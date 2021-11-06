06ba38a0a8ec462370590fedb21be7e4eeba4d10
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2021-11-06 11:12:20 -0500

    Validate the Fields in Contact Edit Form

--- src/cms/contacts/contact-edit/contact-edit.component.ts
+++ src/cms/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-edit',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
-  constructor() { }
+  constructor(private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
+
+  onRemoveItem(i){}
+
+  onCancel(){}
+
+  onSubmit(form: NgForm){}
 
 }
