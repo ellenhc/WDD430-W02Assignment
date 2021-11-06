369e26411be213be81e3ee49d6fedf5b09d00907
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2021-11-05 23:56:35 -0500

    Implementing Behavior of Document Edit

--- src/cms/documents/document-edit/document-edit.component.ts
+++ src/cms/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'app-document-edit',
@@ -6,16 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
-  originalDocument: Document; //  origianl, unedited version
+  originalDocument: Document; //  original, unedited version
   document: Document; // edited version of the document
   editMode: boolean = false; // indicates whether an existing doc is being edited, or a new one is being created
 
-  constructor() { }
+  constructor(private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id = params['id'];
+        if (!params.id) {
+          this.editMode = false;
+          return;
+        } //endif
+        this.originalDocument = this.documentService.getDocument(id);
+
+        if (!this.originalDocument) {
+          return;
+        } //endif
+        this.editMode = true;
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+      })
   }
 
-  onCancel(){}
+  onCancel() {
+    this.router.navigateByUrl('/documents');
+  }
 
-  onSubmit(f){}
+  onSubmit(f: NgForm) {
+    const value = f.value;
+    const newDocument = new Document(value.id, value.name, value.description, value.url, []);
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    }
+    else {
+      this.documentService.addDocument(newDocument);
+    }
+    // route back to '/documents' URL
+    this.router.navigateByUrl('/documents');
+  }
 }
