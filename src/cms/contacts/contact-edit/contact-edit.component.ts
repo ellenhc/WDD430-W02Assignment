9cdd19545bbe9c3caa9f2e7597a2f9210ad8cbf8
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2021-11-06 12:12:45 -0500

    Implement Drag & Drop to Manage Group Contacts

--- src/cms/contacts/contact-edit/contact-edit.component.ts
+++ src/cms/contacts/contact-edit/contact-edit.component.ts
@@ -43,8 +43,6 @@ export class ContactEditComponent implements OnInit {
       })
   }
 
-  onRemoveItem(i) { }
-
   onCancel() {
     this.router.navigateByUrl('/contacts');
   }
@@ -62,4 +60,34 @@ export class ContactEditComponent implements OnInit {
     this.router.navigateByUrl('/contacts');
   }
 
+  isInvalidContact(newContact: Contact) {
+    if (!newContact) {// newContact has no value
+      return true;
+    }
+    if (this.contact && newContact.id === this.contact.id) {
+      return true;
+    }
+    for (let i = 0; i < this.groupContacts.length; i++) {
+      if (newContact.id === this.groupContacts[i].id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addToGroup($event: any) {
+    const selectedContact: Contact = $event.dragData;
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+    if (invalidGroupContact) {
+      return;
+    }
+    this.groupContacts.push(selectedContact);
+  }
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+      return;
+    }
+    this.groupContacts.splice(index, 1);
+  }
 }
